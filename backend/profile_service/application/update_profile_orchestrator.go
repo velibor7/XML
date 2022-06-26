feb600ed3bd0e3a70570a74424db482107c4fd83
sljivkov <sljivkov@gmail.com>
2022-06-26 12:24:46 +0200

    created saga orchestrator

---
+++ backend/profile_service/application/update_profile_orchestrator.go
@@ -0,0 +1,59 @@
+package application
+
+import (
+	saga "github.com/velibor7/XML/common/saga/messaging"
+	events "github.com/velibor7/XML/common/saga/update_profile"
+	"github.com/velibor7/XML/profile_service/domain"
+)
+
+type UpdateProfileOrchestrator struct {
+	commandPublisher saga.Publisher
+	replySubscriber  saga.Subscriber
+}
+
+func NewUpdateProfileOrchestrator(publisher saga.Publisher, subscriber saga.Subscriber) (*UpdateProfileOrchestrator, error) {
+	o := &UpdateProfileOrchestrator{
+		commandPublisher: publisher,
+		replySubscriber:  subscriber,
+	}
+	err := o.replySubscriber.Subscribe(o.handle)
+	if err != nil {
+		return nil, err
+	}
+	return o, nil
+}
+
+func (o *UpdateProfileOrchestrator) Start(profile *domain.Profile, oldUsername string, oldFirstName string, oldLastName string, oldIsPrivate bool) error {
+	event := &events.UpdateProfileCommand{
+		Profile:      *profile,
+		OldUsername:  oldUsername,
+		OldFirstName: oldFirstName,
+		OldLastName:  oldLastName,
+		OldIsPrivate: oldIsPrivate,
+		Type:         events.UpdateProfile,
+	}
+	return o.commandPublisher.Publish(event)
+}
+
+func (o *UpdateProfileOrchestrator) handle(reply *events.UpdateProfileReply) {
+	command := events.UpdateProfileCommand{
+		Profile:      reply.Profile,
+		OldUsername:  reply.OldUsername,
+		OldFirstName: reply.OldFirstName,
+		OldLastName:  reply.OldLastName,
+		OldIsPrivate: reply.OldIsPrivate,
+	}
+	command.Type = o.nextCommandType(reply.Type)
+	if command.Type != events.UnknownCommand {
+		_ = o.commandPublisher.Publish(command)
+	}
+}
+
+func (o *UpdateProfileOrchestrator) nextCommandType(reply events.UpdateProfileReplyType) events.UpdateProfileCommandType {
+	switch reply {
+	case events.ProfileNotUpdated:
+		return events.RollbackUpdatedProfile
+	default:
+		return events.UnknownCommand
+	}
+}
--- backend/profile_service/go.mod
+++ backend/profile_service/go.mod
@@ -22,7 +22,7 @@ require (
 	github.com/xdg-go/scram v1.0.2 // indirect
 	github.com/xdg-go/stringprep v1.0.2 // indirect
 	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
-	golang.org/x/crypto v0.0.0-20201216223049-8b5274cf687f // indirect
+	golang.org/x/crypto v0.0.0-20210314154223-e6e6c4f2bb5b // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
