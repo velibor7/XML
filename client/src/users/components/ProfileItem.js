28c8d91c8f20a28a90ee15d7ded421006ec65b51
PULK4N <nikola_s99@live.com>
2022-07-05 15:41:19 +0200

    front changed

--- client/src/users/components/ProfileItem.js
+++ client/src/users/components/ProfileItem.js
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import "./ProfileItem.css";
+import WorkExperienceItem from "./WorkExperienceItem"
+import EducationItem from "./EducationItem"
 
 import React, { useContext } from "react";
 
@@ -18,12 +20,69 @@ const ProfileItem = (props) => {
           <p className="profile__item__lastName">
             Last name: {props.item.profile?.lastName}
           </p>
+          <p className="profile__item__dateOfBirth">
+            Date of birth: {props.item.profile?.dateOfBirth}
+          </p>
           <p className="profile__item__gender">
             Gender: {props.item.profile?.gender}
           </p>
           <p className="profile__item__biography">
             Biography: {props.item.profile?.biography}
           </p>
+          <p className="profile__item__phoneNumber">
+            Phone number: {props.item.profile?.phoneNumber}
+          </p>
+          <p className="profile__item__email">
+            Email: {props.item.profile?.email}
+          </p>
+          <div className="skills">
+            <h4>Skills</h4>
+            <div className="profile__item__skill">
+              <div className="skill__wrapper">
+                <div className="skill-list__items">
+                  {props.item.profile?.skills.map((item) => (
+                    <p> {item} </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="interests">
+            <h4>Interests</h4>
+            <div className="profile__item__interest">
+              <div className="interest__wrapper">
+                <div className="interest-list__items">
+                  {props.item.profile?.interests.map((item) => (
+                    <p> {item} </p>
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="work_experience">
+            <h4>Work experience</h4>
+            <div className="profile__item__work_experience">
+              <div className="work_experience__wrapper">
+                <div className="work_experience-list__items">
+                  {props.items?.map((item) => (
+                    <WorkExperienceItem item={item} key={item.id} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="education">
+            <h4>Education</h4>
+            <div className="profile__item__educations">
+              <div className="profile__wrapper">
+                <div className="education-list__items">
+                  {props.items?.map((item) => (
+                    <EducationItem item={item} key={item.id} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
--- client/src/users/pages/Users.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Users = (props) => {
-  return <div>Users</div>;
-};
-
-export default Users;
