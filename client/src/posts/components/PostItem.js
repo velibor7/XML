9fbed93161c6daf704ed9a6048873d92564fb3f2
PULK4N <nikola_s99@live.com>
2022-07-10 00:39:15 +0200

    likes implemented

--- client/src/posts/components/PostItem.js
+++ client/src/posts/components/PostItem.js
@@ -15,16 +15,20 @@ const PostItem = (props) => {
   const auth = useContext(AuthContext);
   
   const LikePost = async () => {
+    for(let i=0; i<props.item.post?.likes.length; i++){
+      if(props.item.post?.likes[i].userId == auth.userId){
+        props.item.post?.likes.splice(i,1);
+      }
+    }
+    for(let i=0; i<props.item.post?.dislikes.length; i++){
+      if(props.item.post?.dislikes[i].userId == auth.userId){
+        props.item.post?.dislikes.splice(i,1);
+      }
+    }
+
     try {
-      var body = {
-        id : props.item.post?.id,
-        user_id: props.item.post?.userId,
-        text: "aaaaaaaaaaaa",
-        links: "aaaaaaaaaaaaaaaaaqqqqqq",
-        likes: [{
-          user_id :  auth.userId
-        }]
-      };
+      props.item.post?.likes.push({userId : auth.userId})
+      let body = props.item.post;
       await sendRequest(
         `http://localhost:8000/posts/${props.item.post?.id}`,
         "PUT",
@@ -34,7 +38,7 @@ const PostItem = (props) => {
           Authorization: "token " + auth.token,
         }
       );
-      navigate("/posts/" + props.item.post?.id);
+      navigate("/posts/" + auth.userId +"/" + props.item.post?.id);
     } catch (err) {
       console.log(err);
     }
@@ -42,10 +46,31 @@ const PostItem = (props) => {
   };
 
   const DislikePost = async () => {
+    for(let i=0; i<props.item.post?.likes.length; i++){
+      if(props.item.post?.likes[i].userId == auth.userId){
+        props.item.post?.likes.splice(i,1);
+      }
+    }
+    for(let i=0; i<props.item.post?.dislikes.length; i++){
+      if(props.item.post?.dislikes[i].userId == auth.userId){
+        props.item.post?.dislikes.splice(i,1);
+      }
+    }
+
     try {
-      navigate(`/post/${auth.userId}/update`);
+      props.item.post?.dislikes.push({userId : auth.userId})
+      let body = props.item.post;
+      await sendRequest(
+        `http://localhost:8000/posts/${props.item.post?.id}`,
+        "PUT",
+        JSON.stringify(body),
+        {
+          "Content-Type": "application/json",
+          Authorization: "token " + auth.token,
+        }
+      );
+      navigate("/posts/" + auth.userId +"/" + props.item.post?.id);
     } catch (err) {
-      navigate(`/post/${auth.userId}/update`);
       console.log(err);
     }
   };
